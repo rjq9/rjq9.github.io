ce240195006077dc43103b96587d4da8f5dfb376
rjq9 <69802699+rjq9@users.noreply.github.com>
2021-01-10 12:51:13 -0500

    made original code
    (dont bother checking that, take my word for it)

--- game3.js
+++ game3.js
@@ -18,4 +18,27 @@ function setup(){
 function draw(){
     background(0);
     image(playerImage, player.x, player.y);
+    if (keysPressed["ArrowRight"] && player.dazed <= 0){
+        player.xvelocity += 1;
+    }
+    else if (keysPressed["ArrowLeft"] && player.dazed <= 0){
+        player.xvelocity -= 1;
+        
+    }
+    player.xvelocity *= 0.9;
+    player.x += player.xvelocity;
+    player.dazed = 30;
+
+    if(player.x < 0 || player.x > 1000 - playerImage.width && player.dazed <= 0){
+        player.xvelocity *= -1;
+        player.dazed = 30;
+    }
+}
+
+function keyPressed(){
+    keyPressed[key] = true;
+}
+
+function keyReleased(){
+    keysPressed[key] = false;
 }
