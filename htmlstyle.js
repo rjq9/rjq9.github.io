8178683d19f964c1b6bfb5728dcf2248465c944d
rjq9 <69802699+rjq9@users.noreply.github.com>
2021-08-02 08:30:25 -0400

    unfinished changes

--- htmlstyle.js
+++ htmlstyle.js
@@ -1,25 +1,36 @@
 /*
 TODO: 
-add CSS copyable,
-figure out how to make efficient by only updating necessary text
-somehow fuse bgrupdate() and tcupdate()
+figure out to combine all into 3 functions: update display, update preview, ~~update variables~~ [update variables done]
 */
-bColorList = [255, 255, 255];
-tColorList = [0, 0, 0]; //may want to update this system later using some form of nested array/dictionary
-function bgrupdate(){
-  document.getElementById("accusative").style.backgroundColor = ("rgb("+bColorList[0]+","+bColorList[1]+","+bColorList[2]+")");
-  document.getElementById("bgrvalshow").innerHTML = ("R: " + bColorList[0] + " G: " + bColorList[1] + " B: " + bColorList[2]);
+
+colorValues = {
+  bgr: [255, 255, 255],
+  tc: [0, 0, 0]
 }
-function tcupdate(){
+cssUpdateLocations = {
+  bgrUpdate: 'backgroundColor',
+  tcupdate: 'color'
+}
+function updateDisplay(UpdateLocation){
+  document.getElementById("accusative").style[cssUpdateLocations[UpdateLocation]] = colorValues
+}
+function bgrupdate(){ //TOREMOVE
+  document.getElementById("accusative").style.backgroundColor = ("rgb("+colorValues.bgr[0]+","+colorValues.bgr[1]+","+colorValues.bgr[2]+")");
+  document.getElementById("bgrvalshow").innerHTML = ("R: " + colorValues.bgr[0] + " G: " + colorValues.bgr[1] + " B: " + colorValues.bgr[2]);
+}
+function tcupdate(){ //TOREMOVE
   document.getElementById("accusative").style.color = ("rgb("+tColorList[0]+","+tColorList[1]+","+tColorList[2]+")");
   document.getElementById("tcvalshow").innerHTML = ("R: " + tColorList[0] + " G: " + tColorList[1] + " B: " + tColorList[2]);
 }
 //Since both these functions are very similar, it could probably be condensed further.
-function bgrnv(val, color){ //standing for background new value
-  bColorList[color] = val;
-  document.getElementById("preview").style.backgroundColor = ("rgb("+bColorList[0]+","+bColorList[1]+","+bColorList[2]+")");
+function bgrnv(){ //standing for background new value TOREMOVE
+  x = colorValues.bgr;
+  document.getElementById("preview").style[colorValues[bgrUpdate]] = ("rgb("+bColorList[0]+","+bColorList[1]+","+bColorList[2]+")");
 }
-function tcnv(val, color){
-  tColorList[color] = val;
+function tcnv(){ //TOREMOVE
   document.getElementById("preview").style.color = ("rgb("+tColorList[0]+","+tColorList[1]+","+tColorList[2]+")");
+}
+function updateVars(val, pos, valType){
+  colorValues[valType][pos] = val;
+  
 }
